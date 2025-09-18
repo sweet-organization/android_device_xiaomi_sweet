a802fb0d254aaf219a5b9899ab1e9208c9c0f781
therealmharc <therealmharc@gmail.com>
2025-09-18 16:03:31 +0000

    sweet: nuke parts for good [1/2]
    Revert https://github.com/sweet-organization/android_device_xiaomi_sweet/commit/0de12edaa259b5002299d48ee8287377eb099103 sweet: Add haptic, thermal and refresh rate settings
    Revert https://github.com/sweet-organization/android_device_xiaomi_sweet/commit/8087cf6c2c841d46253eddca36eb0c0f269fb711 sweet: parts: Checkout thermal profiles to xiaomi-sm8250 implementation
    Revert https://github.com/sweet-organization/android_device_xiaomi_sweet/commit/b69bda20c74c6f33c1a8a7e13223cb72de2f0b7e sweet: parts: Fix thermal profile screen state handling
    Revert https://github.com/sweet-organization/android_device_xiaomi_sweet/commit/775987c09e06a27c276e7f7ae412c5e28c30aa09 sweet: parts: Increase BootCompletedReceiver priority
    Revert https://github.com/sweet-organization/android_device_xiaomi_sweet/commit/ec5331e800c9ce623beebe1875a7bcac7d06a70c sweet: parts: checkout to @eidawon implementation
    
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- device.mk
+++ device.mk
@@ -27,10 +27,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_overlay_dynamic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_dynamic.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_overlay_static.xml
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Dolby
 TARGET_USES_DOLBY := true
 
